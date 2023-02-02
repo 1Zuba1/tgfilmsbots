b44b803c27b7f77e872aa9d446c2ed9024f00a25
1Zuba1 <90002760+1Zuba1@users.noreply.github.com>
2023-02-02 23:11:14 +0300

    Add files via upload

---
+++ tgsfilmsbot/config.py
@@ -0,0 +1,11 @@
+﻿#By @K1p1k#
+#Downloaded from TG @KiTools#
+#Leave this inscription#
+
+token='5468861352:AAHDRuamIEm0HM-2Gf3UfLZz-VMtJmsGK78' #https://t.me/BotFather
+admin_id=[819248865] #https://t.me/getidsbot #водить через запетую если не сколько [12321, 3139231]
+rate_searsh=10
+
+#Автор: @K1p1k#
+#Загружено с TG @KiTools#
+#Оставь эту надпись#
